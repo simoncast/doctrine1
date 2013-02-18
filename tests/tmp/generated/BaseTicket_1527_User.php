2e8842f645685addd545051c6f1bc487ae1e6e7e
Mishal <michi.m@gmail.com>
2013-02-18 22:03:07 +0100

    updated generated test

--- tests/tmp/generated/BaseTicket_1527_User.php
+++ tests/tmp/generated/BaseTicket_1527_User.php
@@ -11,7 +11,6 @@
  * @package    ##PACKAGE##
  * @subpackage ##SUBPACKAGE##
  * @author     ##NAME## <##EMAIL##>
- * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
 abstract class BaseTicket_1527_User extends Doctrine_Record
 {
