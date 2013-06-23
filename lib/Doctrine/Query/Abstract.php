67dd49fb2fb4869b21143222592107da738d402a
Simon Cast <simon.cast@gmail.com>
2013-06-23 22:09:44 +0100

    Changed array_diff to internal function to avoid notice errors in PHP 5.4

--- lib/Doctrine/Query/Abstract.php
+++ lib/Doctrine/Query/Abstract.php
@@ -205,13 +205,13 @@ abstract class Doctrine_Query_Abstract
      */
     protected $_queryComponents = array();
 
-	/**
+  /**
      * Stores the root DQL alias
      *
      * @var string
      */
     protected $_rootAlias = '';
-	
+  
     /**
      * @var integer $type                   the query type
      *
@@ -562,7 +562,7 @@ abstract class Doctrine_Query_Abstract
     public function fixArrayParameterValues($params = array())
     {
         $i = 0;
-	
+  
         foreach ($params as $param) {
             if (is_array($param)) {
                 $c = count($param);
@@ -1149,9 +1149,9 @@ abstract class Doctrine_Query_Abstract
         $copy->free();
 
         if ($componentsBefore !== $componentsAfter) {
-            return array_diff($componentsAfter, $componentsBefore);
+          return $this->array_diff_assoc_recursive($componentsAfter,$componentsBefore);                
         } else {
-            return $componentsAfter;
+          return $componentsAfter;
         }
     }
 
@@ -2167,5 +2167,29 @@ abstract class Doctrine_Query_Abstract
     public function setDisableLimitSubquery($disableLimitSubquery)
     {
         $this->disableLimitSubquery = $disableLimitSubquery;
+    }
+
+    protected function array_diff_assoc_recursive($array1,$array2)
+    {
+      $difference = array();
+      foreach($array1 as $key => $value) 
+      {
+        if( is_array($value) ) 
+        {
+          if( !isset($array2[$key]) || !is_array($array2[$key]) ) 
+          {
+            $difference[$key] = $value;
+          } else {
+            $new_diff = $this->array_diff_assoc_recursive($value, $array2[$key]);
+            if( !empty($new_diff) ) 
+            {
+              $difference[$key] = $new_diff;
+            }
+          }
+        } else if( !array_key_exists($key,$array2) || $array2[$key] !== $value ) {
+            $difference[$key] = $value;
+        }
+      }
+      return $difference;        
     }
 }
